1703dba388063a813855b3193b5eac13d215e7ca
kevinccbsg <kevinccbsg@gmail.com>
2018-04-23 22:51:25 -0400

    Router configuration

--- src/components/StackList.js
+++ src/components/StackList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import stacks from '../data/stacks.json';
 
 class StackList extends Component {
@@ -6,7 +7,9 @@ class StackList extends Component {
     return (
       <div>
         {stacks.map(obj => (
-          <h4 key={obj.id}>{obj.title}</h4>
+          <Link key={obj.id} to="/stack">
+            <h4>{obj.title}</h4>
+          </Link>
         ))}
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import App from './components/App';
+import Stack from './components/Stack';
+
 
 render((
-  <App />
+  <Router>
+    <Switch>
+      <Route path="/" exact component={App} />
+      <Route path="/stack" component={Stack} />
+    </Switch>
+  </Router>
 ), document.getElementById('root'))
