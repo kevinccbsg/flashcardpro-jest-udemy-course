8352fd78718d7ac34924aa651e8b33180b7fc206
kevinccbsg <kevinccbsg@gmail.com>
2018-04-24 23:27:31 -0400

    Modified app component to stateless functional component

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import StackList from './StackList';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h2>Flashcard Pro</h2>
-        <StackList />
-        <hr />
-        <Link to="/stackform"><h4>Create a new stack</h4></Link>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div>
+    <h2>Flashcard Pro</h2>
+    <StackList />
+    <hr />
+    <Link to="/stackform"><h4>Create a new stack</h4></Link>
+  </div>
+);
 
 export default App;
