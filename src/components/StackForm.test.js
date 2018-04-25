00479841d8158780638a148842d4e3368d2efaf5
kevinccbsg <kevinccbsg@gmail.com>
2018-04-24 22:36:23 -0400

    Testing write title and add card behaviour

--- src/components/StackForm.test.js
+++ src/components/StackForm.test.js
@@ -19,4 +19,34 @@ describe('StackForm', function() {
   it('render a button to add new car', () => {
     expect(stackform.find('Button').at(0).children().text()).toEqual('Add a card');
   });
+
+  describe('an updating the title', () => {
+    const updateTitle = 'change title';
+    beforeEach(() => {
+      stackform.find('FormControl').simulate('change', {
+        target: {
+          value: updateTitle,
+        }
+      });
+    });
+
+    it('state affected', () => {
+      expect(stackform.state().title).toEqual(updateTitle);
+    });
+  });
+
+  describe('when adding a new card', () => {
+    beforeEach(() => {
+      stackform.find('Button').at(0).simulate('click');
+    });
+
+    it('add new cards to state', () => {
+      expect(stackform.state().cards.length).toEqual(1);
+    });
+
+    it('it renders the prompt section', () => {
+      expect(stackform.find('ControlLabel').at(1).props().children).toEqual('Prompt:');
+    });
+  });
+
 });
