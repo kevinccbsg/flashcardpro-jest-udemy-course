964c18a40df4f846db7ad7f4b76e2d2d09806910
kevinccbsg <kevinccbsg@gmail.com>
2018-04-24 21:52:24 -0400

    Finished app component test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -7,4 +7,13 @@ describe('App', () => {
   it('Renders `Flashcard Pro` title', () => {
     expect(app.find('h2').text()).toEqual('Flashcard Pro');
   });
+
+  it('renders StackLists', () => {
+    // we've gotta be carefully with connect statement for testing
+    expect(app.find('Connect(StackList)').exists()).toBe(true);
+  });
+
+  it('reners a link to create new stacks', () => {
+    expect(app.find('Link h4')).toHaveLength(1);
+  });
 });
