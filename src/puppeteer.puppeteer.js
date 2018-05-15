cd90fdefbfed7a127b39b326c711205ab4b4d08b
kevinccbsg <kevinccbsg@gmail.com>
2018-05-14 23:47:33 -0400

    First integration test demo with puppeteer

--- src/puppeteer.puppeteer.js
+++ src/puppeteer.puppeteer.js
@@ -22,3 +22,34 @@ describe(
   },
   timeout
 );
+
+describe(
+  '/stackform',
+  () => {
+    let page;
+    beforeAll(async () => {
+      page = await global.__BROWSER__.newPage()
+      await page.goto('http://localhost:3000/stackform');
+    }, timeout);
+
+    afterAll(async () => {
+      await page.close();
+    });
+
+    it('should load without error and the correct message', async () => {
+      let text = await page.evaluate(() => document.body.textContent);
+      expect(text).toContain('Create a new Stack');
+    });
+
+    it('should complete the form', async () => {
+      const button = await page.$('.btn.btn-default');
+      await page.type('.form-control', 'ey');
+      await button.tap();
+      let text = await page.evaluate(() => document.body.textContent);
+      await page.screenshot({path: 'test-assets/example.png'});
+      expect(text).toContain('Prompt:');
+    });
+  },
+  timeout
+);
+
